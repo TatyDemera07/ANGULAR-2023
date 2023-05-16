78260bb284a96b38862acc0b82e333745494f52d
TatyDemera07 <102629946+TatyDemera07@users.noreply.github.com>
2023-05-15 23:16:50 -0500

    first commint

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  //rutas protegidas
+  {path: 'dashboard', component: PagesComponent,
+
+
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'categories', component: CategoriesComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  },
+      { path:'', redirectTo:'/dashboard', pathMatch:'full'},
+    ]
+
+  //{path:'**', component: NoFoundPageComponent},
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class PagesRoutingModule { }
